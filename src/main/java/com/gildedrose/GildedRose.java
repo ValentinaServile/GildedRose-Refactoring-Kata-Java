be1f4aa62969302a27c9752694c005de2569bc74
ValentinaServile <valentina.servile@gmail.com>
2016-01-23 13:26:05 +0100

    Extract Sulfuras logic into single if statement

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -16,11 +16,13 @@ class GildedRose {
     			currentItem.sellIn--;
     			continue;
     		}
+    		else if(currentItem.name.equals("Sulfuras, Hand of Ragnaros")) {
+    			continue;
+    		}
+    		
 			if (!currentItem.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (currentItem.quality > 0) {
-                    if (!currentItem.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        currentItem.quality = currentItem.quality - 1;
-                    }
+                    currentItem.quality = currentItem.quality - 1;
                 }
             } else {
                 if (currentItem.quality < 50) {
@@ -42,16 +44,12 @@ class GildedRose {
                 }
             }
 
-            if (!currentItem.name.equals("Sulfuras, Hand of Ragnaros")) {
-                currentItem.sellIn = currentItem.sellIn - 1;
-            }
+            currentItem.sellIn = currentItem.sellIn - 1;
 
             if (currentItem.sellIn < 0) {
                 if (!currentItem.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                     if (currentItem.quality > 0) {
-                        if (!currentItem.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            currentItem.quality = currentItem.quality - 1;
-                        }
+                        currentItem.quality = currentItem.quality - 1;
                     }
                 } else {
                     currentItem.quality = currentItem.quality - currentItem.quality;
