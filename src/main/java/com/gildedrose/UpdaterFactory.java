4e7c4bd549c8731d7513e076f81df1d738c150a1
ValentinaServile <valentina.servile@gmail.com>
2016-01-23 13:58:58 +0100

    Flip equals in UpdaterFactory

--- src/main/java/com/gildedrose/UpdaterFactory.java
+++ src/main/java/com/gildedrose/UpdaterFactory.java
@@ -3,13 +3,13 @@ package com.gildedrose;
 public class UpdaterFactory {
 
 	public static Updater updaterFor(Item currentItem) {
-		if (currentItem.name.equals("Aged Brie")) {
+		if ("Aged Brie".equals(currentItem.name)) {
 			return new AgedBrieUpdater();
 		}
-		else if(currentItem.name.equals("Sulfuras, Hand of Ragnaros")) {
+		else if("Sulfuras, Hand of Ragnaros".equals(currentItem.name)) {
 			return new SulfurasUpdater();
 		}
-		else if(currentItem.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+		else if("Backstage passes to a TAFKAL80ETC concert".equals(currentItem.name)) {
 			return new BackstagePassesUpdater();
 		}
 		else {
